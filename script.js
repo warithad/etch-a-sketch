00e429e27f0413118b41411d6446ac5e99b1dc49
warith <warith.ad02@gmail.com>
2022-09-02 07:11:56 +0100

    Reset grid evenly with fraction

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".container");
 
+// container.setAttribute('style', 'grid-template-columns: repeat(' + val + ',' + times+'px');
+// container.setAttribute('style', 'grid-template-rows: repeat(' + val + ',' + times+'px');
+
+
 for(let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
         const a = document.createElement('div')
