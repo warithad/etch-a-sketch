6123d5aac8bca45cf081cd31523c62502b9bed6a
warith <warith.ad02@gmail.com>
2022-09-18 22:47:19 +0100

    Correct grid bug, Implement functionality of buttons and adjust header and footer

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.getElementById("container");
-// const reset = document.getElementById('reset');
-// const black = document.getElementById('black');
-// const rainbow = document.getElementById('rainbow');
+const reset = document.getElementById('reset');
+const black = document.getElementById('black');
+const rainbow = document.getElementById('rainbow');
 
 
-// var gridSize = 10;
+var r = 0;
+var g = 0;
+var b = 0;
+
+var gridSize = 16;
 
 function setGrid(value){
-    // container.setAttribute('style', 'grid-template-columns: repeat('+value+' 1fr)');
-    // container.setAttribute('style', 'grid-template-rows: repeat('+value+' 1fr)');
-    container.style.gridTemplateColumns = 'repeat(${value}, 1fr)';
-    container.style.gridTemplateRows = 'repear(${value}, 1fr)';
+
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
     console.log(value);
     for(let i = 0; i < value; i++){
         for(let j = 0; j < value; j++){
@@ -20,26 +23,63 @@ function setGrid(value){
             container.appendChild(a);
         }
     }
-
 }
 
-// const boxes = document.querySelectorAll('.box');
-// // function setColorAsBlack(){
-// //     boxes.forEach(box => box.classList.add('black-color'));
-// // }
 
-// reset.addEventListener('click', function() {
-//     prompt('Enter a number');
-// });
+function generateRandomNumber(){
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+}
 
-// black.addEventListener('click', function() {
-//     setColorAsBlack();
-// });
-// window.onload = () =>{
-//     setGrid(5);
-//     boxes.forEach(box => box.classList.add('black-color'));
-// }
+//set grids to random colors
+function setRainbowMode(){
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => 
+        
+        box.addEventListener('mouseover', () => {
+            generateRandomNumber();
+            box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }));
+}
+
+// sets paint to black mode 
+function setBlackMode(){
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => 
+        box.addEventListener('mouseover', () => {
+            box.setAttribute('style', 'background-color: black');
+        }));
+}
+
+// resets paint to white mode
+function resetToDefault(){
+     gridSize = prompt('enter desired grid dimension');
+    // if( typeof gridSize !== Number){
+    //     return;
+    // }
+
+    setGrid(gridSize);
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => box.setAttribute('style', 'background-color: white'));
+}
+
+reset.addEventListener('click', () =>{
+    resetToDefault();
+});
+
+black.addEventListener('click', () =>{
+    setBlackMode();
+});
+
+rainbow.addEventListener('click', () =>{
+    setRainbowMode();
+});
 
 window.onload = () =>{
-    setGrid(10);
+    setGrid(gridSize);
+    setBlackMode();
 }
