28d26421a86cde39a0d3aa9329c53e039be80843
warith <warith.ad02@gmail.com>
2022-09-18 18:50:56 +0100

    Current situation

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-const container = document.querySelector(".container");
+const container = document.getElementById("container");
+// const reset = document.getElementById('reset');
+// const black = document.getElementById('black');
+// const rainbow = document.getElementById('rainbow');
 
-// container.setAttribute('style', 'grid-template-columns: repeat(' + val + ',' + times+'px');
-// container.setAttribute('style', 'grid-template-rows: repeat(' + val + ',' + times+'px');
 
+// var gridSize = 10;
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        const a = document.createElement('div')
-        a.classList.add('box');
-        a.setAttribute('id', i + '_' + j);
-        container.appendChild(a);
+function setGrid(value){
+    // container.setAttribute('style', 'grid-template-columns: repeat('+value+' 1fr)');
+    // container.setAttribute('style', 'grid-template-rows: repeat('+value+' 1fr)');
+    container.style.gridTemplateColumns = 'repeat(${value}, 1fr)';
+    container.style.gridTemplateRows = 'repear(${value}, 1fr)';
+    console.log(value);
+    for(let i = 0; i < value; i++){
+        for(let j = 0; j < value; j++){
+            const a = document.createElement('div');
+            a.classList.add('box');
+            a.setAttribute('id', i + '_' + j);
+            container.appendChild(a);
+        }
     }
+
 }
 
+// const boxes = document.querySelectorAll('.box');
+// // function setColorAsBlack(){
+// //     boxes.forEach(box => box.classList.add('black-color'));
+// // }
 
+// reset.addEventListener('click', function() {
+//     prompt('Enter a number');
+// });
 
+// black.addEventListener('click', function() {
+//     setColorAsBlack();
+// });
+// window.onload = () =>{
+//     setGrid(5);
+//     boxes.forEach(box => box.classList.add('black-color'));
+// }
+
+window.onload = () =>{
+    setGrid(10);
+}
